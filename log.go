4fb4252bf534bad0f664e9df367bb40e4256d0d8
gwakuh <gwakuh@hotmail.com>
2019-10-01 11:39:39 +0900

    fixed all

--- log.go
+++ log.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"io"
 	"os"
 
 	gwformatter "github.com/gwakuh/logrus-gwakuh-formatter"
@@ -8,9 +9,41 @@ import (
 	lumberjack "gopkg.in/natefinch/lumberjack.v2"
 )
 
-var (
-	logger *logrus.Logger
-)
+type Logger struct {
+	FilePath      string `default:"log/app"`
+	FileExtension string `default:".log"`
+	MaxSize       int    `default:"100"` // megabytes
+	MaxBackups    int    `default:"7"`
+	MaxAge        int    `default:"30"` // days
+	Compress      bool   `default:"false"`
+	Logger        *logrus.Logger
+}
+
+func NewWithParams(filePath, fileExtension string, maxSize, maxBackups, maxAge int, compress bool) *Logger {
+	return &Logger{
+		FilePath:      filePath,
+		FileExtension: fileExtension,
+		MaxSize:       maxSize,
+		MaxBackups:    maxBackups,
+		MaxAge:        maxAge,
+		Compress:      compress,
+		Logger:        logrusLogger(lumberjackLogger(filePath+fileExtension, maxSize, maxBackups, maxAge, compress)),
+	}
+}
+
+func New() *Logger {
+	return &Logger{
+		Logger: logrusLogger(os.Stdout),
+	}
+}
+
+func logrusLogger(out io.Writer) *logrus.Logger {
+	return &logrus.Logger{
+		Formatter: &gwformatter.Formatter{},
+		Out:       out,
+		Level:     logrus.TraceLevel, // Trace > Debug > Info > Warn > Error > Fatal > Panic
+	}
+}
 
 func lumberjackLogger(filePath string, maxSize int, maxBackups int, maxAge int, compress bool) *lumberjack.Logger {
 	return &lumberjack.Logger{
@@ -22,34 +55,17 @@ func lumberjackLogger(filePath string, maxSize int, maxBackups int, maxAge int, 
 	}
 }
 
-func initToStdout() {
-	logger = logrus.New()
-	logger.SetFormatter(&gwformatter.Formatter{})
-	logger.SetOutput(os.Stdout)
-	logger.SetLevel(logrus.TraceLevel) // Trace > Debug > Info > Warn > Error > Fatal > Panic
-}
+func (logger *Logger) Log(level string, msg string, args ...interface{}) {
+	l := logger.Logger
 
-func initToFile() {
-	logger = logrus.New()
-	logger.SetFormatter(&gwformatter.Formatter{})
-	out := lumberjackLogger(Config.Log.FilePath+Config.Log.FileExtension, Config.Log.MaxSize, Config.Log.MaxBackups, Config.Log.MaxAge, Config.Log.Compress)
-	logger.SetOutput(out)
-	logger.SetLevel(logrus.TraceLevel) // Trace > Debug > Info > Warn > Error > Fatal > Panic
-}
-
-func init() {
-	initToFile()
-}
-
-func Log(level string, msg string, args ...interface{}) {
 	switch level {
 	case Info:
-		logger.Infof(msg+"\n", args...)
+		l.Infof(msg+"\n", args...)
 	case Warning:
-		logger.Warnf(msg+"\n", args...)
+		l.Warnf(msg+"\n", args...)
 	case Error:
-		logger.Errorf(msg+"\n", args...)
+		l.Errorf(msg+"\n", args...)
 	case Debug:
-		logger.Debugf(msg+"\n", args...)
+		l.Debugf(msg+"\n", args...)
 	}
 }
